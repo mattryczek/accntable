443af01299ac4d9db1dc41a3ef05a642f7c5eadc
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-20 13:55:51 -0500

    Add Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+
+  <div class="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-primary">Primary button</button>
+  </div>
+
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,7 +23,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView /> -->
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 
 import './assets/main.css'
 
+// Import Bootstrap CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
 const app = createApp(App)
 
 app.use(router)
