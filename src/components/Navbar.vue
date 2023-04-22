9dd3416de55b8cab2bff71ba9788c02339667f38
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-22 15:44:53 -0500

    Switch to RouterLink logic

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,12 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">Accntable</a>
+            <RouterLink to="/" class="navbar-brand">Accntable</RouterLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -9,13 +14,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="/properties">Properties</a>
+                        <RouterLink to="/properties" class="nav-link">Properties</RouterLink>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/landlords">Landlords</a>
+                        <RouterLink to="/landlords"  class="nav-link">Landlords</RouterLink>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/about">About</a>
+                        <RouterLink to="/about" class="nav-link">About</RouterLink>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
