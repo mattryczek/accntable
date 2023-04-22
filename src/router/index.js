c2f02e39b2e3b96913a5395dc619e1b58347bd3c
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-22 17:01:34 -0500

    Always load Home view initially, lazy load everything else

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Home from '../views/Home.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/Home.vue')
+      component: Home
     },
     {
       path: '/properties',
