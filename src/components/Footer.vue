d75dd7a928ee4383379b7a4c4f06aa159fc94743
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-23 11:07:24 -0500

    Clean up footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container fixed-bottom">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0 text-body-secondary">© 2023 Accntable</p>
+        </footer>
+    </div>
+</template>
