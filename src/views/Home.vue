0cb5f06a7651e48dae696191fe354767cca406f2
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-23 14:39:38 -0500

    Add navbar back to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <script setup>
 import MainSearch from '@/components/MainSearch.vue';
 import Footer from '@/components/Footer.vue';
+import Navbar from '../components/Navbar.vue';
 
 </script>
 
 <template>
+    <Navbar />
     <div class="home container">
         <MainSearch />
         <Footer />
