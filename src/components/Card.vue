f9248d271050abe9cc720b51705ae7338d11872f
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-23 15:14:42 -0500

    v1 Property Cards

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<!-- 
+    JSON Template for Property Object
+
+    address1: String,
+    address2: String,
+    city: String,
+    manager: String,
+    phone: String,
+    propertyID: Number,
+    state: String,
+    zip: String,
+
+-->
+
+<script setup>
+defineProps({
+    data: Object
+})
+</script>
+
+<template>
+    <div class="card my-3">
+        <h5 class="card-header"> {{ data.address1 + ' ' + data.address2}}</h5>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</template>
--- src/views/Properties.vue
+++ src/views/Properties.vue
@@ -1,12 +1,17 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import Card from '../components/Card.vue';
+
+import propData from '@/assets/json/properties.json'
+
+const properties = propData.properties;
 
 </script>
 
 <template>
   <Navbar />
-  <div class="properties">
-    <h1>This is the page that shows all the properties we have available.</h1>
+  <div class="properties container">
+    <Card v-for="property in properties" :key="property.id" :data=property />
   </div>
 </template>
 
