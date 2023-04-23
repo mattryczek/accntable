89d3e391a9ea8b1e57dd5133667a2073ceb16d71
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-23 14:39:12 -0500

    Search placeholder variable

--- src/components/MainSearch.vue
+++ src/components/MainSearch.vue
@@ -1,12 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const placeholder = ref('Address, Name, Firm or ZIP')
+
+</script>
+
 <template>
     <div class="row justify-content-center">
         <div class="col-9">
             <div id="search" class="d-flex flex-wrap justify-content-center">
                 <div class="p-1 flex-grow-1">
-                    <input class="form-control" type="search" placeholder="Search">
+                    <input class="form-control" type="search" :placeholder="placeholder">
                 </div>
                 <div class="p-1">
-                    <button class="btn btn-outline-secondary me-2">Options</button>
+                    <button class="btn btn-outline-secondary me-2">Filters</button>
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </div>
             </div>
