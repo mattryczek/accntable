32a57d229f8704498af4254ddcc58b980383b934
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-23 11:10:07 -0500

    Remove redundant search from navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,10 +29,6 @@ import { RouterLink } from 'vue-router'
             <RouterLink to="/about" class="nav-link">About</RouterLink>
           </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-outline-success" type="submit">Go</button>
-        </form>
       </div>
     </div>
   </nav>
