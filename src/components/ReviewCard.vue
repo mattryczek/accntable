d8d303da9c986ec545fcbee456d7a447aefbb0a3
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-29 20:38:15 -0500

    v2 ReviewCard

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -1,3 +1,10 @@
+<!-- 
+    "id":
+    "username":
+    "avatar_url": 
+    "review_text":
+    "post_date":-->
+
 <script setup>
 import { ref } from 'vue'
 
@@ -10,18 +17,18 @@ defineProps({
     <div class="card mb-3">
         <div class="row g-0">
             <div class="col-md-2">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Rusty_spotted_cat_1.jpg/440px-Rusty_spotted_cat_1.jpg" class="img-fluid rounded-start" alt="...">
+                <img :src=data.avatar_url class="img-fluid rounded-start">
             </div>
             <div class="col-md-10">
                 <div class="card-body">
-                    <h5 class="card-title">Just a little guy lol</h5>
-                    <p class="card-text">Yeah this place was alright i guess not enough mice tho 7/10</p>
+                    <h5 class="card-title">{{ data.username }}</h5>
+                    <p class="card-text">{{ data.review_text }}</p>
                     <p class="card-text"></p>
                 </div>
                 
             </div>
             <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+                <small class="text-muted">{{ data.post_date }}</small>
             </div>
         </div>
     </div>
