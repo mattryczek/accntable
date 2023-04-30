c3ea83a4c8a3b003d1c6dcf803646401e1931df1
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-29 20:13:40 -0500

    v0 PropertyDetail Page

--- src/views/PropertyDetail.vue
+++ src/views/PropertyDetail.vue
@@ -1,30 +1,59 @@
+<!-- 
+    JSON Index for Property Object
+
+    address1: String,
+    address2: String,
+    city: String,
+    manager: String,
+    phone: String,
+    propertyID: Number,
+    state: String,
+    zip: String,
+    photoURL: String
+
+-->
+
 <script setup>
 // https://stackoverflow.com/a/74286664
 import { useRoute } from 'vue-router'
-const route = useRoute()
+import { ref } from 'vue'
 
 import Navbar from '@comp/Navbar.vue'
-import Carousel from '@comp/Carousel.vue'
-
+import ReviewCard from '@comp/ReviewCard.vue'
 import propData from '@/assets/json/properties.json'
+
+const route = useRoute()
 const properties = propData.properties
 
 let current = properties.find((el) => el.propertyID === route.params.id)
+let photoURL = ref(current.photoURL)
+
 </script>
 
 <template>
   <Navbar />
   <div class="container">
-    <div class="card">
-      <div class="card-body">
-        {{ current.address1 }}
+    <div class="row">
+
+      <div class="col">
+        <img :src=photoURL class="rounded img-fluid">
       </div>
+
+      <div class="col">
+        <h1>{{ current.address1 }}</h1>
+        <h3>{{ current.address2 }}</h3>
+      </div>
+
     </div>
+  </div>
+
+  
+  <div id="reviews" class="container mt-3">
+    <h2>Reviews for this Property</h2>
+    <ReviewCard v-for="property in properties" :key="property.id" :data="property" />
   </div>
 </template>
 
 <style>
-#test {
-  max-width: 50%;
-}
+
 </style>
