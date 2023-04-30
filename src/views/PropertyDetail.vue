bf414d8266c356d759128567ef047da3c049d74e
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-29 20:37:54 -0500

    v1 PropertyDetail page

--- src/views/PropertyDetail.vue
+++ src/views/PropertyDetail.vue
@@ -20,10 +20,13 @@ import { ref } from 'vue'
 
 import Navbar from '@comp/Navbar.vue'
 import ReviewCard from '@comp/ReviewCard.vue'
+
 import propData from '@/assets/json/properties.json'
+import reviewData from '@/assets/json/reviews.json'
 
 const route = useRoute()
 const properties = propData.properties
+const reviews = reviewData.reviews
 
 let current = properties.find((el) => el.propertyID === route.params.id)
 let photoURL = ref(current.photoURL)
@@ -36,21 +39,22 @@ let photoURL = ref(current.photoURL)
     <div class="row">
 
       <div class="col">
-        <img :src=photoURL class="rounded img-fluid">
-      </div>
-
-      <div class="col">
         <h1>{{ current.address1 }}</h1>
         <h3>{{ current.address2 }}</h3>
       </div>
+
+      <div class="col">
+        <img :src=photoURL class="rounded img-fluid">
+      </div>
+
 
     </div>
   </div>
 
   
-  <div id="reviews" class="container mt-3">
-    <h2>Reviews for this Property</h2>
-    <ReviewCard v-for="property in properties" :key="property.id" :data="property" />
+  <div id="reviews" class="container mt-4">
+    <h2 class="border-bottom mb-3">Reviews for this Property</h2>
+    <ReviewCard v-for="review in reviews" :key="review.id" :data="review" />
   </div>
 </template>
 
