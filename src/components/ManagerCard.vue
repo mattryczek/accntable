c8e339fddff1b32af0065c685214a5bf78fcf849
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-30 14:20:50 -0500

    v1 Manager Card

---
+++ src/components/ManagerCard.vue
@@ -0,0 +1,29 @@
+<!-- 
+"id":
+"name":
+"address":
+"city":
+"state":
+"zip":
+"stars":
+"image_url":
+-->
+
+<script setup>
+let props = defineProps({
+  data: Object
+})
+
+let starsInt = parseInt(props.data.stars)
+let rating = '★'.repeat(starsInt) + '☆'.repeat(5 - starsInt)
+</script>
+
+<template>
+  <div class="card my-2 shadow-sm">
+    <div class="card-body">
+      <h5 class="card-title">{{ data.name }}</h5>
+      <h6>{{ rating }}</h6>
+      <pre class="card-text">{{ data.address + '\n' + data.city + ', ' + data.state + ' ' + data.zip }}</pre>
+    </div>
+  </div>
+</template>
