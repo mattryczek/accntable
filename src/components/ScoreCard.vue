85e23adfd805f2389aad150148db953a689f93da
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-30 16:20:30 -0500

    Resolve race condition
    <script setup> is promise based. This caused stars to sometimes
    not show up until a hard reload of the page. We could have
    wrapped the component in a `<suspense>` tag but I instead chose
    to break out the transform() function into one that runs before the
    mustaches resolve.

--- src/components/ScoreCard.vue
+++ src/components/ScoreCard.vue
@@ -9,38 +9,54 @@
 
 <script setup>
 let ratingsObj = defineProps({
-  data: Object
+    data: Object
 })
-
-function transform(ratings) {
-  Object.keys(ratings).forEach(
-    (k) => (ratings[k] = '★'.repeat(ratings[k]) + '☆'.repeat(5 - ratings[k]))
-  )
-}
 
 let ratings = ratingsObj.data
 
-transform(ratings)
+function transform(number) {
+    return '★'.repeat(number) + '☆'.repeat(5 - number)
+}
 </script>
 
 <template>
-  <div class="card">
-    <h5 class="card-header">Detailed Ratings</h5>
-    <div class="card-body d-flex flex-wrap">
-      <div class="col-md-6 my-2">
-        <p class="card-text">
-          Responsiveness 
-          <br />
-          <span class="text-muted">{{ ratings.responsiveness }}</span>
-        </p>
-        <p class="card-text">Friendliness {{ ratings.friendliness }}</p>
-        <p class="card-text">Management {{ ratings.management }}</p>
-      </div>
-      <div class="col-md-6 my-2">
-        <p class="card-text">Maintenance {{ ratings.maintenance }}</p>
-        <p class="card-text">Rental Rate {{ ratings.rental_rate }}</p>
-        <p class="card-text">Communication {{ ratings.communication }}</p>
-      </div>
+    <div class="card">
+        <h5 class="card-header">Detailed Ratings</h5>
+        <div class="card-body d-flex flex-wrap">
+            <div class="col-md-6 my-2">
+                <p class="card-text">
+                    Responsiveness
+                    <br />
+                    <span>{{ transform(ratings.responsiveness) }}</span>
+                </p>
+                <p class="card-text">
+                    Friendliness
+                    <br />
+                    <span>{{ transform(ratings.friendliness) }}</span>
+                </p>
+                <p class="card-text">
+                    Management
+                    <br />
+                    <span>{{ transform(ratings.management) }}</span>
+                </p>
+            </div>
+            <div class="col-md-6 my-2">
+                <p class="card-text">
+                    Maintenance
+                    <br />
+                    <span>{{ transform(ratings.maintenance) }}</span>
+                </p>
+                <p class="card-text">
+                    Rental Rate
+                    <br />
+                    <span>{{ transform(ratings.rental_rate) }}</span>
+                </p>
+                <p class="card-text">
+                    Communication
+                    <br />
+                    <span>{{ transform(ratings.communication) }}</span>
+                </p>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
