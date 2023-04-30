63036387f11cc95a29cf4da6cc4387f8c68ddb4f
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-30 15:24:16 -0500

    ReviewCard Improvements

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -16,19 +16,15 @@ defineProps({
 <template>
     <div class="card mb-3">
         <div class="row g-0">
-            <div class="col-md-2">
+            <div class="col-sm-2">
                 <img :src=data.avatar_url class="img-fluid rounded-start">
             </div>
-            <div class="col-md-10">
+            <div class="col-sm-10">
                 <div class="card-body">
-                    <h5 class="card-title">{{ data.username }}</h5>
+                    <h5 class="card-title">{{ data.username }} | <small class="text-muted">{{ data.post_date }}</small></h5>
                     <p class="card-text">{{ data.review_text }}</p>
-                    <p class="card-text"></p>
                 </div>
                 
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">{{ data.post_date }}</small>
             </div>
         </div>
     </div>
