1ce9edfdc04866c0632826f8e04a7397eda17ce4
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-30 15:22:49 -0500

    v0 ScoreCard

---
+++ src/components/ScoreCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+let props = defineProps({
+  data: Object
+})
+
+
+</script>
+
+<template>
+    <div class="card">
+        <h5 class="card-header">Detailed Ratings</h5>
+        <div class="card-body d-flex flex-wrap">
+            <div class="col-md-6">
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            <div class="col-md-6">
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
+</template>
