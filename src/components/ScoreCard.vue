0d542068d67850ea311569de50be0bb1d0bdb6c4
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-04-30 16:03:35 -0500

    ScoreCard Star Transform Logic

--- src/components/ScoreCard.vue
+++ src/components/ScoreCard.vue
@@ -12,18 +12,28 @@ let ratingsObj = defineProps({
   data: Object
 })
 
+function transform(ratings){
+    Object.keys(ratings).forEach(k => ratings[k] = '★'.repeat(ratings[k]) + '☆'.repeat(5 - ratings[k]))
+}
+
 let ratings = ratingsObj.data
+
+transform(ratings)
 </script>
 
 <template>
     <div class="card">
         <h5 class="card-header">Detailed Ratings</h5>
         <div class="card-body d-flex flex-wrap">
-            <div class="col-md-6">
-                <p class="card-text">{{ ratings.responsiveness }}</p>
+            <div class="col-md-6 my-2">
+                <p class="card-text">Responsiveness {{ ratings.responsiveness }}</p>
+                <p class="card-text">Friendliness {{ ratings.friendliness }}</p>
+                <p class="card-text">Management {{ ratings.management }}</p>
             </div>
-            <div class="col-md-6">
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <div class="col-md-6 my-2">
+                <p class="card-text">Maintenance {{ ratings.maintenance }}</p>
+                <p class="card-text">Rental Rate {{ ratings.rental_rate }}</p>
+                <p class="card-text">Communication {{ ratings.communication }}</p>
             </div>
         </div>
     </div>
