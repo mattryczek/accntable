fa9e1bdf1c4165474b3369e5b5b7658cf0ff0cb7
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-05-14 17:06:30 -0500

    Improve date readability

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -2,13 +2,17 @@
 defineProps({
   data: Object
 })
+
+function transform(iso_string){
+  return new Date(iso_string).toLocaleDateString()
+}
 </script>
 
 <template>
   <div class="card mb-3">
     <div class="card-body">
       <h5 class="card-title">
-        {{ data.author }} | <small class="text-muted">{{ data.created_at }}</small>
+        {{ data.author }} | <small class="text-muted">{{ transform(data.created_at) }}</small>
       </h5>
       <p class="card-text">{{ data.notes }}</p>
     </div>
